ce41579bce7df1390a09bf8f4f9a19783c53bc36
Ana Cecilia Grossi Bolito <anac.grossi@icloud.com>
2023-03-08 08:51:26 -0300

    Add project files.

---
+++ ProjExDoisString/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string texto1, texto2;
+        Console.WriteLine("Digite seu primeiro texto: ");
+        texto1 = Console.ReadLine();
+        texto1 = texto1.Trim(' ');
+        Console.WriteLine("Digite seu segundo texto: ");
+        texto2 = Console.ReadLine();
+        texto2 = texto2.Trim(' ');
+
+
+        if (texto1 == texto2)
+            Console.WriteLine("Os textos são iguais.");
+        else
+            Console.WriteLine("Os textos não são iguais.");
+    }
+}
